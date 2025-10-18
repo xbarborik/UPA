828d15a7cc6ddac41ad150072190daf11beb9d8c
xbarborik <martinbarborik9@gmail.com>
2025-10-18 19:47:27 +0200

    Ignores duplicates

--- part1/urls.py
+++ part1/urls.py
@@ -4,6 +4,7 @@ from urllib.request import urlopen
 def getTabletUrls(number_of_products_to_scrape = 150):
   pageNumber = 1
   number_of_urls_acquired = 0
+  urls = set()
 
   while number_of_urls_acquired < number_of_products_to_scrape:
     url = "https://www.laptopsdirect.co.uk/ct/tablet-pcs-and-e-readers/tablets?pageNumber=" + str(pageNumber)
@@ -14,12 +15,15 @@ def getTabletUrls(number_of_products_to_scrape = 150):
     a_tags = product_list.find_all("a", class_="offerboxtitle")
 
     for a_tag in a_tags:
-      product_url = "https://www.laptopsdirect.co.uk/" + a_tag["href"]
-      print(product_url)
+      url = "https://www.laptopsdirect.co.uk/" + a_tag["href"]
+      if url in urls:
+        continue
+      print(url)
       number_of_urls_acquired += 1
+      urls.add(url)
       if (number_of_urls_acquired) >= number_of_products_to_scrape:
         break
-        
+
     pageNumber += 1
 
 if __name__ == "__main__":
