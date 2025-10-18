e973c59002031ce2a1fd3881f4fef18e68620d70
xbarborik <martinbarborik9@gmail.com>
2025-10-18 19:08:21 +0200

    Simplified looking for urls

--- part1/urls.py
+++ part1/urls.py
@@ -11,16 +11,14 @@ def getTabletUrls(number_of_products_to_scrape = 150):
     html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
     product_list = soup.select_one("#productList")
-    rows = product_list.find_all("div", class_="OfferBox")
+    a_tags = product_list.find_all("a", class_="offerboxtitle")
 
-    for row in rows:
-      a_tag = row.find("a", href=True)
-      if a_tag:
-        product_url = "https://www.laptopsdirect.co.uk/" + a_tag["href"]
-        print(product_url)
-        number_of_urls_acquired += 1
-        if (number_of_urls_acquired) >= number_of_products_to_scrape:
-          break
+    for a_tag in a_tags:
+      product_url = "https://www.laptopsdirect.co.uk/" + a_tag["href"]
+      print(product_url)
+      number_of_urls_acquired += 1
+      if (number_of_urls_acquired) >= number_of_products_to_scrape:
+        break
         
     pageNumber += 1
 
